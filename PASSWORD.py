fb62f35eeb0ea52144d60790b8533697d9a03372
danielwatsonfire <danielwatson281@yahoo.co.uk>
2018-08-02 15:49:19 +0100

    Added files

---
+++ PASSWORD.py
@@ -0,0 +1,10 @@
+userName=input("Enter UserName")
+password=input("Enter Password NOW")
+if userName=="User":
+    else userName!="User":
+        print("NOT YOU")
+    if password=="Ipod123":
+        print("You're IN")
+    elif password != "Ipod123":
+            print("HA" + "HA!")
+
---
+++ task.py
@@ -0,0 +1,25 @@
+
+#Examples of variables
+while True:
+    Variable_one="Avenged"
+    Variable_two="SevenFold"
+    Variable_three="Tour"
+    myInt="2018"
+    myStr="Newcastle"
+    Space=" "
+    print(Variable_one)
+    print(Variable_two)
+    print(myStr + Space + myInt)
+    # User input of sums
+    num1 = input("number one: ")
+    num2 = input("number two: ")
+    sum = float(num1)+ float(num2)
+    print(sum)
+    #multi-line comment
+    print('''This
+    is a multi
+    Line Comment''')
+    userName=input("what is your name?")
+
+    print( "DILLIGAF" + userName)
+    print(" This Sum is:" +str(sum))
